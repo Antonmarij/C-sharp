21b116f1b54a0beac5356b87dc5b4bba366bd236
Anton Marijanovic <anton_m00@hotmail.com>
2023-09-01 14:34:12 +0200

    La till mapp för hemarbete

--- Hem_Kod/Hem_Kod/Program.cs
+++ Hem_Kod/Hem_Kod/Program.cs
@@ -2,3 +2,39 @@
  ----------------
 */
 
+List<string> tasks = new List<string>();
+
+while (true)
+{
+    Console.WriteLine("1. Add Task");
+    Console.WriteLine("2. View Tasks");
+    Console.WriteLine("3. Exit");
+    Console.Write("Select an option: ");
+    int choice = Convert.ToInt32(Console.ReadLine());
+
+    switch (choice)
+    {
+        case 1:
+            Console.Write("Enter a new task: ");
+            string newTask = Console.ReadLine();
+            tasks.Add(newTask);
+            Console.WriteLine("Task added!");
+            break;
+
+        case 2:
+            Console.WriteLine("Tasks:");
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {tasks[i]}"); // Fix the typo here, add closing curly brace and quotes.
+            }
+            break;
+
+        case 3:
+            Console.WriteLine("Exiting...");
+            return;
+
+        default:
+            Console.WriteLine("Invalid choice. Please select a valid option.");
+            break;
+    }
+}
