6274d8a680e02bf95978ce8edec51cc769203243
Anton Marijanovic <anton_m00@hotmail.com>
2023-09-01 18:40:52 +0200

    Försöker göra en discord bot

---
+++ Discord_bot_5/Discord_bot_5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+class Program
+{
+    private DiscordSocketClient _client;
+
+    public static async Task Main(string[] args)
+    {
+        var program = new Program();
+        await program.RunBotAsync();
+    }
+
+    public async Task RunBotAsync()
+    {
+        _client = new DiscordSocketClient();
+
+        _client.Log += LogAsync;
+        _client.Ready += ReadyAsync;
+
+        await _client.LoginAsync(TokenType.Bot, MTE0NzIwMDgyNzA1Mjc4OTgzMQ.GIa1EX.6WYll6bcXaUa7dWkPnvmr - P4eCn6N56aNx4fvg);
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+
+    private Task LogAsync(LogMessage log)
+    {
+        Console.WriteLine(log);
+        return Task.CompletedTask;
+    }
+
+    private async Task ReadyAsync()
+    {
+        Console.WriteLine("Bot is connected and ready!");
+
+        // Your bot's code goes here.
+    }
+}
