6a5cbeeeeef704a04efec6c7c4311cea507715f5
Anton Marijanovic <anton_m00@hotmail.com>
2023-09-04 15:57:45 +0200

    working version of bot

---
+++ Discord_bot_surelythistime/Discord_bot_surelythistime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+class Program
+{
+
+    static async Task Main(string[] args)
+    {
+        var discord = new DiscordClient(new DiscordConfiguration()
+        {
+            Token = Environment.GetEnvironmentVariable("Discord_Bot_Token"),
+            TokenType = TokenType.Bot,
+            Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+        });
+        
+        discord.MessageCreated += async (s, e) =>
+        {
+            if (e.Message.Content.ToLower().StartsWith("ping"))
+                await e.Message.RespondAsync("pong!"); 
+        };
+        await discord.ConnectAsync();
+        await Task.Delay(-1);
+    }
+}
