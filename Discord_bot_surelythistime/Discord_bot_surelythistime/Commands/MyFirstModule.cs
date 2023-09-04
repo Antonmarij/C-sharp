fd2f4d7c5f2781f4ad0a8353ac29c0c444cccd6c
Anton Marijanovic <anton_m00@hotmail.com>
2023-09-04 16:52:10 +0200

    name commands here

---
+++ Discord_bot_surelythistime/Discord_bot_surelythistime/Commands/MyFirstModule.cs
@@ -0,0 +1,17 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MyFirstModule : BaseCommandModule
+{
+    [Command("greet")]
+    public async Task GreetCommand(CommandContext ctx)
+    {
+        await ctx.RespondAsync("Greetings!");
+    }
+}
--- Discord_bot_surelythistime/Discord_bot_surelythistime/Program.cs
+++ Discord_bot_surelythistime/Discord_bot_surelythistime/Program.cs
@@ -15,13 +15,25 @@ class Program
         });
         
         discord.MessageCreated += async (s, e) =>
-        {
+        {//name commands
             if (e.Message.Content.ToLower().StartsWith("!dovla"))
                 await e.Message.RespondAsync("is stupid!");
             if (e.Message.Content.ToLower().StartsWith("!moop"))
                 await e.Message.RespondAsync("is the cutest boy ever :3");
             if (e.Message.Content.ToLower().StartsWith("!hyph"))
                 await e.Message.RespondAsync("is the only smart brit out there lul");
+            if (e.Message.Content.ToLower().StartsWith("!dotty"))
+                await e.Message.RespondAsync("she's busy in battlegrounds sorry");
+            if (e.Message.Content.ToLower().StartsWith("!floyz"))
+                await e.Message.RespondAsync("who, the bollywood pooper?");
+            if (e.Message.Content.ToLower().StartsWith("!swep"))
+                await e.Message.RespondAsync("busy in his 16th split of the week sry");
+            if (e.Message.Content.ToLower().StartsWith("!stitch"))
+                await e.Message.RespondAsync("is sleepwalking but awake");
+            if (e.Message.Content.ToLower().StartsWith("!rogue"))
+                await e.Message.RespondAsync("Sims:\nhttps://wowsims.github.io/wotlk/rogue/\n BisList:\n<https://wowtbc.gg/wotlk/bis-list/combat-rogue/>");
+
+      
         };
         await discord.ConnectAsync();
         await Task.Delay(-1);
