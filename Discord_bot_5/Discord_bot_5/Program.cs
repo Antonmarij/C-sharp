3706b6da77ab5599edb2ef3bb0b81d913d9e46ad
Anton Marijanovic <anton_m00@hotmail.com>
2023-09-04 15:05:04 +0200

    Ej fungerande version av discord bot

--- Discord_bot_5/Discord_bot_5/Program.cs
+++ Discord_bot_5/Discord_bot_5/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 public class Program
@@ -49,11 +50,21 @@ public class CommandHandler
         var message = messageParam as SocketUserMessage;
         if (message == null) return;
         int argPos = 0;
-        if (!(message.HasCharPrefix('!', ref argPos) ||
+        if (!(message.HasCharPrefix('!', ref argPos) || 
             message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
             message.Author.IsBot)
             return;
         var context = new SocketCommandContext(_client, message);
         await _commands.ExecuteAsync(context: context, argPos: argPos, services: null);
     }
+}
+public class InfoModule : ModuleBase<SocketCommandContext>
+{
+    //For a command to be valid, it must have a return type of Task or Task<RuntimeResult>
+    // say dovla is silly -> dovla is silly
+    [Command("say")]
+    [Summary("Echoes a message.")]
+    public Task SayAsync([Remainder][Summary("The text to echo")] string echo)
+        => ReplyAsync(echo);
+
 }
