2891c85ef5cae22d13d5ebd481433b2145480dcb
Anton Marijanovic <anton_m00@hotmail.com>
2023-10-02 14:54:46 +0200

    ui todolist

---
+++ Lektion-10/ToDoList/ToDoListApp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using ToDoListApp.Models;
+
+namespace ToDoListApp;
+
+public partial class MainPage : ContentPage
+{
+    ObservableCollection<Todo> Todos = new ObservableCollection<Todo>(); 
+
+
+    public MainPage()
+    {
+        InitializeComponent();
+        TodoList.ItemsSource = Todos;
+    }
+    private void AddButton_Clicked(object sender, EventArgs e)
+    {
+        Todos.Add(new Todo { Activity = ToDoActivity.Text });
+        ToDoActivity.Text = string.Empty;
+    }
+    private void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        var button = (Button)sender;
+        var todo = button.BindingContext as Todo;
+
+        Todos.Remove(todo);
+    }
+}
---
+++ Lektion-10/ToDoList/ToDoListApp/Models/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoListApp.Models;
+//9 question
+public class Todo
+{
+    public string Activity { get; set; } = null!;
+    public DateTime Created { get; set; } =DateTime.Now;
+
+}
